70d6c200476756bbf62f7797e3a6d8014a10e71d
Elijah Shaw-Rutschman <elijahr@gmail.com>
2019-12-26 17:08:04 -0600

    PEP8 fix

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ from setuptools.command.install import install
 from setuptools.command.sdist import sdist
 
 
-DARWIN_INCLUDE = '/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/'
+DARWIN_INCLUDE = '/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform' \
+                 '/Developer/SDKs/MacOSX.sdk/usr/include/'
 
 
 def install_libc_headers_and(cmdclass):
